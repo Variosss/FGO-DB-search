a193096ade0ccfdf1ef23726d7d952163225ef59
Jakub Grąbka <variosPL@outlook.com>
2022-10-16 21:11:37 +0200

    Initial Commit

---
+++ FGO DB search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FGO DB search
+//
+//  Created by Jakub Grąbka on 16/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FGO DB search/FGO_DB_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FGO_DB_searchApp.swift
+//  FGO DB search
+//
+//  Created by Jakub Grąbka on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FGO_DB_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
